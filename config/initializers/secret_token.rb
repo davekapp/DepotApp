cfa88cd03f780fc79e449d12b3c83a7430581505
Dave K <davekapp@gmail.com>
2010-09-05 01:04:25 -0600

    initial depot being put under git control

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4eb34b86126c2ad401e39c219b959f0a82077fbd4fd4dc11b9d271368c3aa18e553a0f37c277c071c8795da144cd535bfc9489b6554fca3d8cb819382219887f'
