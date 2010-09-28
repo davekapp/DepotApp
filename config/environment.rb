89dc16ab9bbbe8f24a040007100a32f90aa4052a
Dave K <davekapp@gmail.com>
2010-09-27 21:30:33 -0600

    fixed bad file move... very bad file move

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# no real mail for now
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test  
+end
+
 # Initialize the rails application
 Depot::Application.initialize!
